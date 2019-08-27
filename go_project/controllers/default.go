649d524811081cb0dd6ad786b6635a65ef3aa185
rockykumar433@gmail.com <rockykumar433@gmail.com>
2019-08-27 16:28:44 +0530

    .gitignore fix

--- go_project/controllers/default.go
+++ go_project/controllers/default.go
@@ -7,9 +7,18 @@ import (
 type MainController struct {
 	beego.Controller
 }
+type UserController struct {
+	beego.Controller
+}
 
 func (c *MainController) Get() {
 	c.Data["Website"] = "beego.me"
 	c.Data["Email"] = "astaxie@gmail.com"
 	c.TplName = "index.tpl"
+}
+
+func (c *UserController) Get() {
+	c.Data["Website"] = "codechair.com"
+	c.Data["Email"] = "rocky@codechair.com"
+	c.TplName = "index1.tpl"
 }
--- go_project/routers/router.go
+++ go_project/routers/router.go
@@ -2,9 +2,11 @@ package routers
 
 import (
 	"go_project/controllers"
+
 	"github.com/astaxie/beego"
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+	beego.Router("/", &controllers.MainController{})
+	beego.Router("/user", &controllers.UserController{})
 }
